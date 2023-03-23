d7be61e81196311048322d1e306fb4ffde854574
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-03-23 16:05:11 +0600

    Paint object

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:travelo/misc/colors.dart';
 import 'package:travelo/widgets/large_text.dart';
 
 class HomePage extends StatefulWidget {
@@ -53,6 +54,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                   unselectedLabelColor: Colors.grey,
                   isScrollable: true,
                   indicatorSize: TabBarIndicatorSize.label,
+                  indicator: CircleTabIndicator(color: AppColors.mainColor, radius: 4),
                   tabs: [
                     Tab(text: "Places"),
                     Tab(text: "Inspirations",),
@@ -75,4 +77,31 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
       ),
     );
   }
+}
+
+class CircleTabIndicator extends Decoration{
+  final Color color;
+  double radius;
+  CircleTabIndicator({required this.color, required this.radius});
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    // TODO: implement createBoxPainter
+    return _CirclePainter(color: color, radius:radius);
+  }
+
+}
+class _CirclePainter extends BoxPainter{
+  final Color color;
+  double radius;
+  _CirclePainter({required this.color, required this.radius});
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    Paint _paint = Paint();
+    _paint.color=color;
+    _paint.isAntiAlias=true;
+    final Offset circleOffset = Offset(configuration.size!.width/2 -radius/2, configuration
+    .size!.height-radius);
+    canvas.drawCircle(offset+circleOffset, radius, _paint);
+  }
+
 }
