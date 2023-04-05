3fd6dc174c85ce121bbc7215752b896713acf80b
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-04-05 13:35:37 +0600

    Google fonts added

--- lib/widgets/app_text.dart
+++ lib/widgets/app_text.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppText extends StatelessWidget {
   double size;
   final String text;
   final Color color;
   final FontStyle? fontStyle;
+  final String fontFamily;
 
   AppText({Key? key,
     this.size=16,
     required this.text,
-    this.color=Colors.black54, this.fontStyle}) : super(key: key);
+    this.color=Colors.black54, this.fontStyle, this.fontFamily= 'Roboto'}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: TextStyle(
-          color: color,
-          fontSize: size,
-          fontStyle: fontStyle,
+        style: GoogleFonts.getFont(
+        fontFamily,
+        textStyle: TextStyle(
+        color: color,
+        fontSize: size,
+        fontStyle: fontStyle,
+        ),
       ),
     );
   }
--- lib/widgets/large_text.dart
+++ lib/widgets/large_text.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LargeText extends StatelessWidget {
   double size;
   final String text;
   final Color color;
   final FontStyle? fontStyle;
+  final String fontFamily;
+
   LargeText({Key? key,
     this.size=30,
     required this.text,
-    this.color=Colors.black, this.fontStyle}) : super(key: key);
+    this.color=Colors.black, this.fontStyle, this.fontFamily= 'Righteous'}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: TextStyle(
-        color: color,
-        fontSize: size,
-        fontWeight: FontWeight.bold,
-        fontStyle: fontStyle,
+      style: GoogleFonts.getFont(
+        fontFamily,
+        textStyle: TextStyle(
+          color: color,
+          fontSize: size,
+          fontStyle: fontStyle,
+        ),
       ),
     );
   }
